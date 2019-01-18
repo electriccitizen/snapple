a1737bd0b4ba630e9848b82e54de14c38e449ee9
broeker <tim@electriccitizen.com>
2019-01-18 16:52:14 -0600

    timemejs test

--- web/modules/custom/snap/js/time_custom.js
+++ web/modules/custom/snap/js/time_custom.js
@@ -3,26 +3,13 @@
         attach: function (context, settings) {
 
             var timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
-            var timeInSeconds = TimeMe.getTimeOnCurrentPageInSeconds();
 
             window.addEventListener("beforeunload", function(event) {
-                ga(‘send’, ‘event’, [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
-                ga(‘send’, ‘event’, ‘timeonpage’, ‘Play’, ‘Product Demo Video’);
                 xmlhttp=new XMLHttpRequest();
-                xmlhttp.open("POST","www.google-analytics.com", true);
+                xmlhttp.open("POST","http://www.google-analytics.com/collect?", true);
                 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                var timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
-                xmlhttp.send("v=1&tid=UA-131852001-1&cid=555&t=pageview&cd5=".timeSpentOnPage);
-                console.log('fooman');
+                xmlhttp.send("v=1&tid=UA-131852001-1&cid=555&t=pageview&cd5=" + timeSpentOnPage);
             });
-
-            window.onbeforeunload = function (event) {
-               // xmlhttp=new XMLHttpRequest();
-               // xmlhttp.open("POST","ENTER_URL_HERE", true);
-               // xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                var timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
-                alert(timeSpentOnPage)
-            };
 
         }
     };
