f8b611bfd46a902097ea985d4e25b13935ba51f0
broeker <tim@electriccitizen.com>
2019-01-21 13:33:17 -0600

    update xhttp request

--- web/modules/custom/snap/js/time_custom.js
+++ web/modules/custom/snap/js/time_custom.js
@@ -1,19 +1,21 @@
-(function ($, Drupal) {
-    Drupal.behaviors.myModuleBehavior = {
+
+(function ($) {
+    Drupal.behaviors.page_load_progress = {
         attach: function (context, settings) {
-
-            var timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
-
-            window.addEventListener("beforeunload", function(event) {
+            window.onbeforeunload = close_event_function;
+            function close_event_function() {
+                var timeInSeconds = TimeMe.getTimeOnCurrentPageInSeconds();
+                console.log('see ya:' + timeInSeconds);
                 xmlhttp=new XMLHttpRequest();
                 xmlhttp.open("POST","http://www.google-analytics.com/collect?", true);
                 xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-                xmlhttp.send("v=1&tid=UA-131852001-1&cid=555&t=pageview&metric1=666cd5=999");
-            });
-
+                xmlhttp.send("v=1&tid=UA-131852001-1&cid=555&t=pageview&cd6=foomanchu&metric1=" + timeInSeconds +"&cd5=" + timeInSeconds +"&cd7=" + timeInSeconds + "&cd8="+ timeInSeconds);
+            }
         }
     };
-})(jQuery, Drupal);
+}(jQuery));
+
+
 
 
 
